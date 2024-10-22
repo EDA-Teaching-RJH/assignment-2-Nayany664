edd579cc859a145feaf178216a92cc919708e46b
Nayany664 <nm664@kent.ac.uk>
2024-10-22 23:18:52 +0000

    forloop (1)

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,5 @@
 ### Part Two -- your code goes here. 
+
+#random number 1-100 guessed by user
+#keep guessing (that number not change of number) until guessed
+
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,11 @@
 ### Part One -- your code goes here. 
+
+#list of numbers from 1-10
+#square it and keep going until last num in list done
+
+numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+
+for number in numbers :
+    square = number * number
+    print( "the square of ", number , "*" , number , " is ", square )
+    number = number + 1
