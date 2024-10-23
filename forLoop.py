13d2a8d8e89375fb5944bfdf53836730b715cb1f
Nayany664 <nm664@kent.ac.uk>
2024-10-23 11:53:04 +0000

    hi

--- forLoop.py
+++ forLoop.py
@@ -8,4 +8,3 @@ numbers = [1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
 for number in numbers :
     square = number * number
     print( "the square of ", number , "*" , number , " is ", square )
-    number = number + 1
